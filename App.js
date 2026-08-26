86797e137368cc31d66ffb836be9025d8058fad5
miappnav-cell <miappnav@gmail.com>
2026-08-25 20:46:59 -0400

    fix: Restaurar configuracion de app.json, nombre de app y verificacion de nodos src

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
-  return <RootNavigator />;
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#090a0f" />
+      <RootNavigator />
+    </>
+  );
 }
